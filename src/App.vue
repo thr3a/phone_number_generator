9d0edb4c312bde9b7efe029bfcd519ed411f906c
thr3a <12jk172@ms.dendai.ac.jp>
2018-05-19 17:59:43 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+const mydata = require('./data.json')
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      message: mydata
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
